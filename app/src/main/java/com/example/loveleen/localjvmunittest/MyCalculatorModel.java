506cf2ddc0b4dc789711ba3dd0dc26a5e050e61b
loveleen <loveleen.nancy@gmail.com>
2018-12-22 10:12:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loveleen/localjvmunittest/MyCalculatorModel.java
@@ -0,0 +1,28 @@
+package com.example.loveleen.localjvmunittest;
+
+public class MyCalculatorModel {
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+
+    public static String divide(int a, int b){
+        if(a==0 && b==0){
+            return "NOT DEFINED";
+        }
+        else if(b==0){
+            return "INFINITY";
+        }
+        else {
+            return String.valueOf(a/b);
+        }
+    }
+
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+}
